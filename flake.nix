a216426bbe4f8ec5c40b8ee281db75aa2f9b44b0
trasha <sasha.friis@gmail.com>
2025-10-01 20:42:57 +0200

    updated

--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-
 {
   description = "Trasha's NixOS + Home Manager flake";
 
@@ -6,15 +5,19 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, home-manager, ... }:
 
-    # NixOS system configuration
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    # NixOS configuration
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+        system = system;
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager
@@ -22,13 +25,13 @@
       };
     };
 
-    # Home Manager user configuration
+    # Home Manager configuration (top-level)
     homeConfigurations = {
       trasha = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        pkgs = pkgs;
         username = "trasha";
         homeDirectory = "/home/trasha";
-        configuration = import ./home.nix;
+        configuration = import ./home.nix { pkgs = pkgs; };
       };
     };
   };
