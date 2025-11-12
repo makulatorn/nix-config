a9078126a3ef5c81695a2fcf3b9c61f9575c50e6
trasha <sasha.friis@gmail.com>
2025-11-12 13:59:48 +0100

    rewrote the wholething + bluetooth

--- configuration.nix
+++ configuration.nix
@@ -1,43 +1,32 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, lib, ... }:
 
 {
+  # --- Session PATH ---
   environment.sessionVariables = {
     PATH = "$HOME/.nix-profile/bin:/run/current-system/sw/bin:$PATH";
   };
 
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  # --- Imports ---
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  # Bootloader.
+  # --- Nix settings ---
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.trusted-users = ["root" "trasha"];
+
+  # --- Bootloader ---
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  # --- Networking ---
+  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
-
-  # Enable network manager applet
   programs.nm-applet.enable = true;
 
-  # Set your time zone.
+  # --- Time & Locale ---
   time.timeZone = "Europe/Copenhagen";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_DK.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "da_DK.UTF-8";
     LC_IDENTIFICATION = "da_DK.UTF-8";
@@ -50,40 +39,25 @@
     LC_TIME = "da_DK.UTF-8";
   };
 
-  # Enable the X11 windowing system.
+  # --- Disable X11 ---
   services.xserver.enable = true;
-
-  virtualisation.docker = {
+  services.xserver.layout = "dk";
+  services.displayManager.sddm = {
     enable = true;
-  };  
-
-  services.xserver.videoDrivers = [ "displaylink" "modesetting"];
-  boot.extraModulePackages = with config.boot.kernelPackages; [ evdi ];
-
-  services.xserver.displayManager.sessionCommands = ''
-    ${lib.getBin pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource 2 0
-  '';
-
-
-  # Enable the LXQT Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  #services.xserver.desktopManager.xfce.enable = true;
-  
-  # Enable Qtile
-  services.xserver.windowManager.qtile.enable = true;
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "dk";
-    variant = "nodeadkeys";
+    wayland.enable = false;
   };
+  services.xserver.windowManager.qtile.enable = true;
+  services.xserver.videoDrivers = [ "displaylink" "modesetting" ];
+  boot.kernelModules = [ "evdi" ];
 
-  # Configure console keymap
+  # --- Sway Wayland ---
+  programs.sway.enable = false;
+
+  # --- Console ---
   console.keyMap = "dk-latin1";
 
-  # Enable CUPS to print documents.
+  # --- Printing & Sound ---
   services.printing.enable = true;
-
-  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -91,42 +65,36 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  nix.settings.trusted-users = ["root" "trasha"];
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  hardware.bluetooth.enable = true;
+  services.blueman. enable = true;
+  # --- Users ---
   users.users.trasha = {
     isNormalUser = true;
     description = "trasha";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
   };
-  environment.variables = {
-  QT_STYLE_OVERRIDE = "adwaita-dark";
-  };
-  # Install firefox.
-  programs.firefox.enable = true;
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-  
-  programs.gnupg.agent= {
+  environment.variables = {
+    QT_STYLE_OVERRIDE = "adwaita-dark";
+  };
+
+  # --- Programs ---
+  programs.firefox.enable = true;
+  programs.gnupg.agent = {
     enable = true;
     pinentryPackage = pkgs.pinentry-curses;
   };
+  virtualisation.docker.enable = true;
+  
+  # --- Allow unfree packages ---
+  nixpkgs.config.allowUnfree = true;
+  xdg.portal.config = {
+    gtk = true;
+  };
 
-  # List packages installed in system profile. To search, run:
+  # --- System packages ---
   environment.systemPackages = with pkgs; [
     wget
     adwaita-qt
@@ -144,8 +112,12 @@
     pkgs.gnumake
     pkgs.pkg-config
     vintagestory
+    waybar
+    xdg-desktop-portal
+    xdg-desktop-portal-gtk
+    google-chrome
 
-    #dev
+    # dev
     python312Full
     python312Packages.pip
     python312Packages.django
@@ -161,7 +133,7 @@
     podman
     podman-compose
     git
-    docker    
+    docker
     redis
     postgresql
     pango
@@ -171,7 +143,8 @@
     rustc
     cargo
 
-    #tools
+    # tools
+    wdisplays
     arandr
     autoconf
     automake
@@ -187,47 +160,32 @@
     nodePackages.npm
     nodejs
     gnupg
-    pinentry-curses 
+    pinentry-curses
     pass
- ];
-   fonts = {
+  ];
+
+  # --- Fonts ---
+  fonts = {
     fontconfig.enable = true;
     packages = with pkgs; [
-    nerd-fonts.fira-code
+      nerd-fonts.fira-code
     ];
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  # --- OpenSSH ---
+  services.openssh.enable = true;
+  services.openssh.settings.PermitRootLogin = "no";
+  services.openssh.settings.PasswordAuthentication = false;
+  services.openssh.ports = [ 2222 ];
 
-  # List services that you want to enable:
+  # --- Firewall ---
+  networking.firewall.allowedTCPPorts = [ 2222 ];
 
-  # Enable the OpenSSH daemon.
-    services.openssh.enable = true;
-    services.openssh.settings.PermitRootLogin = "no";
-    services.openssh.settings.PasswordAuthentication = false;
-    services.openssh.ports = [ 2222 ];
+  # --- State version ---
+  system.stateVersion = "25.05";
 
-  # Open ports in the firewall.
-    networking.firewall.allowedTCPPorts = [ 2222 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
-  
-  services.picom={
+  # --- Picom (still enabled if needed) ---
+  services.picom = {
     enable = true;
     package = pkgs.picom;
   };
