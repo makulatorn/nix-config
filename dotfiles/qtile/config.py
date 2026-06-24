30ee80500c685ec092f070d72d83d3129da1240c
sasha <sasha.friis@icloud.com>
2026-06-24 18:25:41 +0200

    follow mouse focus false

--- dotfiles/qtile/config.py
+++ dotfiles/qtile/config.py
@@ -244,7 +244,7 @@ mouse = [
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: list
-follow_mouse_focus = True
+follow_mouse_focus = False
 bring_front_click = False
 floats_kept_above = True
 cursor_warp = True
